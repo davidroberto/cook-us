9d7096d5d17b43aa1d9bf84e0c338dd98dedc892
Lucas <lucasleveque17@gmail.com>
2026-03-02 16:54:17 +0100

    feat[frontend]: ajout du layout backoffice avec sidebar et routing React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
-import { UsersTablePage } from '@/modules/users/usersTable.page'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BackofficeLayout } from '@src/layouts/backoffice.layout'
+import { UsersTablePage } from '@src/modules/users/usersTable.page'
+import { CookRequestsTablePage } from '@src/modules/cookRequests/cookRequestsTable.page'
 
 export const App = () => {
-    return <UsersTablePage />
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<BackofficeLayout />}>
+                    <Route index element={<Navigate to="/users" replace />} />
+                    <Route path="/users" element={<UsersTablePage />} />
+                    <Route path="/cook-requests" element={<CookRequestsTablePage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
