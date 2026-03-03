1356b0d4d01ed98ed409f92eececab1d4cce7e09
mdorizon <mdorizon@proton.me>
2026-03-03 09:34:57 +0100

    🐛

--- frontend/src/contexts/auth.context.tsx
+++ frontend/src/contexts/auth.context.tsx
@@ -1,15 +1,7 @@
-import { createContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import type { AuthUser } from '@src/types/auth.types'
 import { mockMe } from '@src/modules/auth/auth.mock'
-
-type AuthContextValue = {
-    user: AuthUser | null
-    isLoading: boolean
-    login: (user: AuthUser) => void
-    logout: () => void
-}
-
-export const AuthContext = createContext<AuthContextValue | null>(null)
+import { AuthContext } from '@src/contexts/auth-context'
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     const [user, setUser] = useState<AuthUser | null>(null)
