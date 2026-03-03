1125f4bd96b8003ce97ed02c70de4caf67c59895
mdorizon <mdorizon@proton.me>
2026-03-03 09:15:28 +0100

    📝 added swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "@src/app.module";
 
 async function bootstrap() {
@@ -7,6 +8,16 @@ async function bootstrap() {
   app.setGlobalPrefix("api");
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle("Cook'us API")
+    .setDescription("API du projet Cook'us")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, document);
+
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
