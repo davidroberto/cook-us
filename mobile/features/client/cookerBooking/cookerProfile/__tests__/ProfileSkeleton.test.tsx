c239482a5b3854bf3b1d47de5c9685f400480ac9
WEZ3R <marcyrius6@gmail.com>
2026-03-03 10:41:00 +0100

    ✨ chore: remove unnecessary peer dependencies from package-lock files

--- mobile/features/client/cookerBooking/cookerProfile/__tests__/ProfileSkeleton.test.tsx
+++ mobile/features/client/cookerBooking/cookerProfile/__tests__/ProfileSkeleton.test.tsx
@@ -1,7 +1,20 @@
+import { Animated } from "react-native";
 import { render, screen } from "@testing-library/react-native";
 import { ProfileSkeleton } from "../components/ProfileSkeleton";
 
 describe("ProfileSkeleton", () => {
+  beforeEach(() => {
+    jest.spyOn(Animated, "loop").mockReturnValue({
+      start: jest.fn(),
+      stop: jest.fn(),
+      reset: jest.fn(),
+    } as any);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
   it("est rendu avec le testID profile-skeleton", () => {
     render(<ProfileSkeleton />);
     expect(screen.getByTestId("profile-skeleton")).toBeTruthy();
